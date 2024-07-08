4569eb53c04ae839f1c3d5a606a594d21cb6cec6
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-07-08 20:25:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ API_ID = int(os.environ.get("API_ID", "24687182"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH", ,"38a36451cc5fb8634af8d2adcd40d780")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7398593479:AAGBpDPNXKSK2G3Jo5z6N_n9SuCw_D4bIYY")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "chinna")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID"))
 # ------------------X------------------------------
