2a795d9b3270dcaa3746c0b6fd728a9a4f68d23b
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-07-08 20:23:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID", "24687182"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH")
 # ----------------D--------------------------------
